5129be4be3fd6fc482dee0b4b3fdc91fa507dc03
'Mualle Mats'ela <mualle@outlook.com>
2016-06-24 16:56:11 +0200

    Added Control_Event_Handlers

--- WpfMvvm/WpfMvvm/MainWindow.xaml.cs
+++ WpfMvvm/WpfMvvm/MainWindow.xaml.cs
@@ -24,5 +24,81 @@ namespace WpfMvvm
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Executed when  window loads
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Executed when Button New Clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void rbtnMale_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rntFemale_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtGrossSalary_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void rbnCloseApp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbtnHelp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }

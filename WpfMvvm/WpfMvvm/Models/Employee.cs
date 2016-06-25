3df81e050ba586bce00a380eb98b481039f89218
'Mualle Mats'ela <mualle@outlook.com>
2016-06-25 02:58:10 +0200

    Introduce Model Classes and View Models

---
+++ WpfMvvm/WpfMvvm/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvm.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool IsMale { get; set; }
+        public string Gender { get { return IsMale ? "Male" : "Female"; } }
+        public string Department { get; set; }
+        public decimal Gross { get; set; }
+        public decimal Payee { get; set; }
+        public decimal Net { get { return Gross - Payee; } }
+        public bool HasPensionFund { get; set; }
+
+    }
+}
---
+++ WpfMvvm/WpfMvvm/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvm.ViewModels
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnpropertyChanged(string property)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+    }
+}

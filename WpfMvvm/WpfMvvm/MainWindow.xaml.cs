3ad77b650be448d42f061f2cbbc9b802dc58d2a2
'Mualle Mats'ela <mualle@outlook.com>
2016-06-25 02:43:17 +0200

    Spaghetti Completed

--- WpfMvvm/WpfMvvm/MainWindow.xaml.cs
+++ WpfMvvm/WpfMvvm/MainWindow.xaml.cs
@@ -27,6 +27,10 @@ namespace WpfMvvm
     {
         private ObservableCollection<Department> _departments;
         private ObservableCollection<Employee> _employees;
+        private List<Employee> _tempEmployees;
+
+
+        bool isNew = false;
 
         public MainWindow()
         {
@@ -35,6 +39,8 @@ namespace WpfMvvm
             InitDepartmentsLookup();
 
             InitEmployeeList();
+
+
 
             this.Closing += MainWindow_Closing;
             btnCancel.Click += BtnCancel_Click;
@@ -54,8 +60,8 @@ namespace WpfMvvm
 
         private void BtnCancel_Click(object sender, RoutedEventArgs e)
         {
-            btnNew_Click(null, null);
-            grpBoxEmployeeDetails.IsEnabled = false;
+            ClearControls();
+            DisableControls();
         }
 
 
@@ -67,10 +73,10 @@ namespace WpfMvvm
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //Disable emmployee group box
-            grpBoxEmployeeDetails.IsEnabled = false;
-
+            DisableControls();
 
         }
+
 
         /// <summary>
         /// Executed when Button New Clicked
@@ -79,35 +85,54 @@ namespace WpfMvvm
         /// <param name="e"></param>
         private void btnNew_Click(object sender, RoutedEventArgs e)
         {
-            grpBoxEmployeeDetails.IsEnabled = true;
-            dgvEmployees.SelectedIndex = -1;
-
-            txtEmployeeNo.Clear();
-            txtFirstName.Clear();
-            txtLastName.Clear();
-            rbtnMale.IsChecked = true;
-            cmbDepartments.SelectedIndex = -1;
-            txtGrossSalary.Text = "0.00";
-
+            isNew = true;
+            ClearControls();
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
+            if (dgvEmployees.SelectedIndex > -1)
+            {
+                isNew = false;
+                grpBoxEmployeeDetails.IsEnabled = true;
+            }
 
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-
+            if (dgvEmployees.SelectedIndex > -1)
+            {
+                if (MessageBox.Show("Do you really want to delete selected employe?", "Confirm:", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    _employees.RemoveAt(dgvEmployees.SelectedIndex);
+                    ClearControls();
+                    DisableControls();
+                }
+            }
         }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
+            if (Validate())
+            {
+                if (isNew)
+                {
+                    AddNew();
 
+                }
+                else
+                {
+                    Update();
+
+                }
+
+            }
         }
 
         private void btnRefresh_Click(object sender, RoutedEventArgs e)
         {
+            RefreshList();
 
         }
 
@@ -118,7 +143,26 @@ namespace WpfMvvm
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            grpBoxEmployeeDetails.IsEnabled = false;
 
+            if (dgvEmployees.SelectedIndex > -1)
+            {
+
+                var employee = _employees[dgvEmployees.SelectedIndex];
+
+                txtEmployeeNo.Text = employee.Id.ToString();
+                txtFirstName.Text = employee.FirstName;
+                txtLastName.Text = employee.LastName;
+                rbtnMale.IsChecked = employee.Gender == "Male";
+                rntFemale.IsChecked = employee.Gender == "Female";
+                cmbDepartments.SelectedValue = employee.Department;
+                txtGrossSalary.Text = decimal.Round(employee.Gross, 2).ToString();
+
+            }
+            else
+            {
+                ClearControls();
+            }
         }
 
         private void rbtnMale_Checked(object sender, RoutedEventArgs e)
@@ -133,7 +177,22 @@ namespace WpfMvvm
 
         private void txtGrossSalary_TextChanged(object sender, TextChangedEventArgs e)
         {
+            try
+            {
+                decimal gross = 0;
+                decimal.TryParse(txtGrossSalary.Text, out gross);
+                decimal paye = CalculatePAYE(gross);
 
+                txtPayee.Text = decimal.Round(paye, 2).ToString();
+
+                decimal net = gross - paye;
+                txtNetSalary.Text = (decimal.Round(net, 2)).ToString();
+
+            }
+            catch
+            {
+
+            }
         }
 
         private void rbnCloseApp_Click(object sender, RoutedEventArgs e)
@@ -166,6 +225,171 @@ namespace WpfMvvm
         {
             _employees = new ObservableCollection<Employee>();
             dgvEmployees.ItemsSource = _employees;
+
+            _tempEmployees = new List<Employee>();
+        }
+
+        private bool Validate()
+        {
+            bool valid = true;
+
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text))
+            {
+                valid = false;
+                MessageBox.Show("First name is required!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            else if (string.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                valid = false;
+                MessageBox.Show("Surname is required!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            else if (cmbDepartments.SelectedIndex <= -1)
+            {
+                valid = false;
+                MessageBox.Show("Employee department is required!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                decimal gross = 0;
+                if (!decimal.TryParse(txtGrossSalary.Text, out gross))
+                {
+                    valid = false;
+                    MessageBox.Show("Gross salary must be anumber not aplphanumeric", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    if (gross <= 0)
+                    {
+                        valid = false;
+                        MessageBox.Show("Please ener gross salary greater than zero!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+
+            return valid;
+        }
+
+        private void ClearControls()
+        {
+            grpBoxEmployeeDetails.IsEnabled = true;
+            dgvEmployees.SelectedIndex = -1;
+
+            txtEmployeeNo.Clear();
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            rbtnMale.IsChecked = true;
+            cmbDepartments.SelectedIndex = -1;
+            txtGrossSalary.Text = "0.00";
+            chkHasPensionFund.IsChecked = false;
+        }
+
+        private void DisableControls()
+        {
+            grpBoxEmployeeDetails.IsEnabled = false;
+        }
+
+        private void RefreshList()
+        {
+            _tempEmployees.Clear();
+            foreach (var emp in _employees)
+            {
+                _tempEmployees.Add(emp);
+            }
+
+            _employees.Clear();
+            foreach (var emp in _tempEmployees)
+            {
+                _employees.Add(emp);
+            }
+        }
+
+        private void Update()
+        {
+            var selectedIndex = dgvEmployees.SelectedIndex;
+            Employee selectedEmployee = _employees[selectedIndex];
+            selectedEmployee.FirstName = txtFirstName.Text;
+            selectedEmployee.LastName = txtLastName.Text;
+            selectedEmployee.Gender = rbtnMale.IsChecked == true ? "Male" : "Female";
+            selectedEmployee.Department = cmbDepartments.SelectedValue.ToString();
+            selectedEmployee.Gross = decimal.Round(decimal.Parse(txtGrossSalary.Text), 2);
+            selectedEmployee.Payee = decimal.Parse(txtPayee.Text);
+            selectedEmployee.HasPensionFund = chkHasPensionFund.IsChecked == true;
+
+            RefreshList();
+
+            ClearControls();
+            DisableControls();
+
+            dgvEmployees.SelectedIndex = selectedIndex;
+        }
+
+        private void AddNew()
+        {
+            _employees.Add(new Employee
+            {
+                FirstName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+                Gender = rbtnMale.IsChecked == true ? "Male" : "Female",
+                Department = cmbDepartments.SelectedValue.ToString(),
+                Gross = decimal.Round(decimal.Parse(txtGrossSalary.Text), 2),
+                Payee = decimal.Parse(txtPayee.Text),
+                HasPensionFund = chkHasPensionFund.IsChecked == true
+
+            });
+
+            ClearControls();
+
+            DisableControls();
+        }
+
+        private decimal CalculatePAYE(decimal taxableAmount)
+        {
+            try
+            {
+                double minPercentage = 0.2; // 20%
+                double maxPercentage = 0.3; // 30%
+                decimal minAmount = decimal.Parse("2805");
+                decimal maxAmount = decimal.Parse("4747");
+
+                decimal GovRen = decimal.Parse(minPercentage.ToString()) * decimal.Parse(minAmount.ToString());
+                GovRen = decimal.Round(GovRen, 2);
+
+                decimal PAYE = 0;
+
+                if (taxableAmount <= minAmount)
+                {
+                    PAYE = 0;
+                }
+                else if (minAmount < taxableAmount && taxableAmount <= maxAmount)
+                {
+                    PAYE = taxableAmount * decimal.Parse(minPercentage.ToString());
+                    PAYE = decimal.Round(PAYE, 2) - GovRen;
+                }
+                else
+                {
+                    decimal Rem = taxableAmount - decimal.Parse(maxAmount.ToString());
+                    Rem = decimal.Round(Rem, 2);
+
+                    decimal LowerRate = decimal.Parse(maxAmount.ToString()) * decimal.Parse(minPercentage.ToString());
+                    decimal UpperRate = decimal.Parse(maxPercentage.ToString()) * Rem;
+
+                    LowerRate = decimal.Round(LowerRate, 2);
+                    UpperRate = decimal.Round(UpperRate, 2);
+
+                    PAYE = (LowerRate + UpperRate) - GovRen;
+                }
+                return PAYE;
+            }
+            catch
+            {
+
+                return 0;
+            }
+
+
+
         }
 
         #endregion
